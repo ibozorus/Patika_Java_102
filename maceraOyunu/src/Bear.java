b4c48a2a7437b585f3368d811850aad8f833fdb6
ibozorus <korkmazie@gmail.com>
2023-03-17 17:14:30 +0100

    Bear ve Zombie classlari olusturuldu

---
+++ maceraOyunu/src/Bear.java
@@ -0,0 +1,6 @@
+public class Bear extends Obstacle{
+
+    private final int damage = 7;
+    private int health = 20;
+    private final int reward = 12;
+}
---
+++ maceraOyunu/src/Zombie.java
@@ -0,0 +1,7 @@
+public class Zombie extends Obstacle{
+
+
+    private final int damage = 3;
+    private int health = 10;
+    private final int reward = 4;
+}
